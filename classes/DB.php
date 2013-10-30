7238b00c7412441d5777ee3ac2717319e8fe10e2
Oleg25 <velmisoff123@gmail.com>
2013-10-30 10:47:25 +0400

    Second Edition

--- classes/DB.php
+++ classes/DB.php
@@ -10,7 +10,7 @@ require_once 'MDB2.php';
 			curl_setopt($curl, CURLOPT_URL, $url);
 			curl_setopt($curl, CURLOPT_TIMEOUT, 3);
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			
+
 
 					$feedContent = curl_exec($curl);
 						curl_close($curl);
@@ -21,106 +21,106 @@ require_once 'MDB2.php';
 
 class DB {
 
-	
+
 	public	$dsn = 'mysql://RKH_meteo:roza_xutor_1@172.17.100.11/meteo';
     public $options = array(
     'debug' => 2,
     'result_buffering' => false,
      );
 
-        protected static $_instance; 
+        protected static $_instance;
 
- 
-        public static function getInstance() { 
-            if (self::$_instance === null) { 
-                self::$_instance = new self;  
+
+        public static function getInstance() {
+            if (self::$_instance === null) {
+                self::$_instance = new self;
             }
-            return self::$_instance; 
+            return self::$_instance;
         }
-        
-		 
+
+
         private  function __construct() { // конструктор отрабатывает один раз при вызове DB::getInstance();
-				   
+
              $mdb2 = MDB2::connect($this->dsn, $this->options);
                 if (PEAR::isError($mdb2)) {
                  die("Невозможно установить соединение c БД ".$mdb2->getMessage());
                  }
 			 $this->connect = $mdb2;
 		$mdb2->setCharset('utf8');
-		
-		    $this->query('SET NAMES utf8');	                
-                           
+
+		    $this->query('SET NAMES utf8');
+
                  }
- 
-        private function __clone() { 
+
+        private function __clone() {
         }
-       
+
         private function __wakeup() {
         }
-		
+
 	  /*function __destruct() {
-	   
+
 	   $obj=self::$_instance;
        $v1 = $obj->connect;
 	    $v1->disconnect();
 		}*/
-               
-      			
-   
+
+
+
         public static function query($sql) {
-				       
+
             $obj=self::$_instance;
-       
+
             if(isset($obj->connect)){
-						                
+
                 $result=mysql_query($sql)or die("<br/><span style='color:red'>Ошибка в SQL запросе:</span> ".mysql_error());
-                              
+
                 return $result;
             }
             return false;
-        }  
-   
-        
+        }
+
+
         public static function fetch_object($object)
         {
             return @mysql_fetch_object($object);
         }
 
-        
+
         public static function fetch_array($object)
         {
-		
+
             return @mysql_fetch_array($object);
-        } 
-		
+        }
+
       public static function disconn()
         {
             $obj=self::$_instance;
             $v1 = $obj->connect;
 	        $v1->disconnect();
         }
-		
-		
+
+
   }
   class Conditions {
 
    public function getTodayConditions(){
-   
+
       DB::getInstance(); //получаем единственный экземпляр(Singleton)класса для подключения к БД
-             
-      $result=DB::query("CALL meteo_rosa();");//вызываем хранимую процедуру(перенос части логики на СУБД) в mySQL  
+
+      $result=DB::query("CALL meteo_rosa();");//вызываем хранимую процедуру(перенос части логики на СУБД) в mySQL
 	  $rows = mysql_num_rows($result);
-	   				
+
 	  for ($j=0; $j<$rows; $j++)
 	            {
-	  
+
 	     $meteo[] = DB::fetch_array($result);// получаем данные в виде массива в скалярную переменую $meteo
 
-                }		 				 
-        
+                }
+
       DB::disconn();//отключаемся от БД
-  	  return $meteo; 
-    }      
+  	  return $meteo;
+    }
 
  }
 ?>
